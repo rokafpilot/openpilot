2f9293cb8c45ce5ffc50aa9bc44686a07606a260
jc01rho <rkjnice@gmail.com>
2020-08-31 22:44:31 +0900

    fix ui

--- selfdrive/ui/paint.cc
+++ selfdrive/ui/paint.cc
@@ -1505,9 +1505,9 @@ static void bb_ui_draw_L_Extra(UIState *s)
     snprintf(str, sizeof(str), "Sat: %d", scene->lqr_saturated ? 1 : 0);
     ui_draw_text(s->vg, text_x, y, str, 25 * 2.5, COLOR_WHITE_ALPHA(200), s->font_sans_regular);
 
-    y += height;
-    snprintf(str, sizeof(str), "CURV: %.3f", scene->pCurvature * 1000.);
-    ui_draw_text(s->vg, text_x, y, str, 25 * 2.5, COLOR_WHITE_ALPHA(200), s->font_sans_regular);
+//    y += height;
+//    snprintf(str, sizeof(str), "CURV: %.3f", scene->pCurvature * 1000.);
+//    ui_draw_text(s->vg, text_x, y, str, 25 * 2.5, COLOR_WHITE_ALPHA(200), s->font_sans_regular);
 
     y += height;
     snprintf(str, sizeof(str), "sR: %.3f", scene->lp_steerRatio);
