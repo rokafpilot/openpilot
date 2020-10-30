bcee0d4df9ca0290a6c36e0c2b7fc987651879ce
whrho <whrho@fasoo.com>
2020-10-30 15:17:09 +0900

    update lqr variables

--- selfdrive/car/gm/interface.py
+++ selfdrive/car/gm/interface.py
@@ -108,9 +108,9 @@ class CarInterface(CarInterfaceBase):
       ret.lateralTuning.lqr.a = [0., 1., -0.22619643, 1.21822268]
       ret.lateralTuning.lqr.b = [-1.92006585e-04, 3.95603032e-05]
       ret.lateralTuning.lqr.c = [1., 0.]
-      ret.lateralTuning.lqr.k = [-103., 450.]
-      ret.lateralTuning.lqr.l = [0.22, 0.318]
-      ret.lateralTuning.lqr.dcGain = 0.003
+      ret.lateralTuning.lqr.k = [-110., 451.]
+      ret.lateralTuning.lqr.l = [0.33, 0.318]
+      ret.lateralTuning.lqr.dcGain = 0.00225
 
 
       tire_stiffness_factor = 1.0
